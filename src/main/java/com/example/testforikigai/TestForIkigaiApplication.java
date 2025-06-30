8f9a99d444ad24cbd9c87f7508c04cf1100f9853
mariiadubinenko <dubinenkomaria@gmail.com>
2025-06-30 18:10:14 +0200

    added some maven dependencies tor tests and parametrized tests

--- src/main/java/com/example/testforikigai/TestForIkigaiApplication.java
+++ src/main/java/com/example/testforikigai/TestForIkigaiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestForIkigaiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestForIkigaiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestForIkigaiApplication.class, args);
+    }
 
 }
