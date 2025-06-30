4885f0209cea456794635bbfb58bb02c7a29379c
mariiadubinenko <dubinenkomaria@gmail.com>
2025-06-30 18:04:04 +0200

    Initial commit

---
+++ src/test/java/com/example/testforikigai/TestForIkigaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testforikigai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestForIkigaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
